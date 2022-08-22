d4cc09b9873aa67afe0fc411f91744fcfa70ff6a
Scala Steward <scala_steward@virtuslab.com>
2022-08-22 14:27:41 +0000

    Revert commit(s) f139d81

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,6 +2,6 @@ import sbt._
 
 object Dependencies {
   lazy val pythonNativeLibs = "ai.kien" %% "python-native-libs" % "0.2.2"
-  lazy val sbtScalaNative = "org.scala-native" % "sbt-scala-native" % "0.3.9"
+  lazy val sbtScalaNative = "org.scala-native" % "sbt-scala-native" % "0.3.7"
   lazy val organizeImports = "com.github.liancheng" %% "organize-imports" % "0.6.0"
 }
